0273e55130332aca0944d1d881285e03bdb74005
yotsy <zobazw@gmail.com>
2026-01-16 21:21:40 +0300

    build: fix: fix unclosed mkdir string

--- Source/ProtoBridgeThirdParty/scripts/build_android.sh
+++ Source/ProtoBridgeThirdParty/scripts/build_android.sh
@@ -19,7 +19,7 @@ FINAL_INCLUDE_DIR="$ROOT_DIR/includes"
 mkdir -p "$BUILD_DIR_HOST"
 mkdir -p "$BUILD_DIR"
 mkdir -p "$FINAL_LIB_DIR"
-mkdir -p "$FINAL_INCLUDE_DIR
+mkdir -p "$FINAL_INCLUDE_DIR"
 
 cd "$SCRIPT_DIR"
 
