69ab5c61a881db78a1b1d5f4592c24daaef0d511
yotsy <zobazw@gmail.com>
2026-01-17 19:43:01 +0300

    ci(bridge_generator): add cmake build scripts

---
+++ Source/ProtoBridgeGenerator/scripts/build_mac.sh
@@ -0,0 +1,7 @@
+﻿#!/bin/bash
+set -e
+
+cd "$(dirname "$0")/.."
+
+cmake -S . -B Build -DCMAKE_BUILD_TYPE=Release
+cmake --build Build --config Release
---
+++ Source/ProtoBridgeGenerator/scripts/build_win.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = "Stop"
+Push-Location "$PSScriptRoot/.."
+
+cmake -S . -B Build -DCMAKE_BUILD_TYPE=Release
+cmake --build Build --config Release
+
+Pop-Location
